be41d7d7ed49cf9d340b9105b8ba42b5c1938076
Rishi-242 <166038865+Rishi-242@users.noreply.github.com>
2025-07-15 19:52:15 +0530

    Fibonacci series

---
+++ fibrec.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+int i,n,a[100];
+printf("enter the value");
+scanf("%d",&n);
+int f=fib(n);
+int fib(int);
+for(i=0;i<n;i++)
+{
+printf("%d",fib(i));
+}
+}
+int fib(int n)
+{
+if(n==0)
+{
+return 0;
+}
+else if(n==1)
+{
+return 1;
+}
+else
+{
+return fib(n-1)+fib(n-2);
+}
+}
